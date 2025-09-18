0cbbd26a4516450a878952d1e96d4b02e817a32c
Evgeniy <qwestwo@gmail.com>
2025-09-18 08:17:45 +0300

    Animation
    Add idle and run animation

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -3,17 +3,44 @@ using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private float _speed = 5f;
-    private float _moveInput;
+    [SerializeField] private float _speed = 6f;
+    [SerializeField] private float _jumpForce = 8f;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float _moveThreshold = 0.01f;
 
-    public void OnMove(InputValue value)
+    private static readonly int RunAnim = Animator.StringToHash("Run");
+
+    private Rigidbody2D _rb;
+    private float _moveInput;
+    private float _originalScaleX;
+
+    private void Awake()
     {
-        _moveInput = value.Get<Vector2>().x;
+        _rb = GetComponent<Rigidbody2D>();
+        _rb.freezeRotation = true;
+        _originalScaleX = Mathf.Abs(transform.localScale.x);
     }
 
     private void Update()
     {
-        transform.Translate(new Vector2(_moveInput, 0) * (_speed * Time.deltaTime));
+        _moveInput = 0f;
+        if (Keyboard.current != null)
+        {
+            if (Keyboard.current.aKey.isPressed) _moveInput = -1f;
+            else if (Keyboard.current.dKey.isPressed) _moveInput = 1f;
+        }
     }
-}
 
+    private void FixedUpdate()
+    {
+        if (Mathf.Abs(_moveInput) > _moveThreshold)
+        {
+            float scaleX = _moveInput > 0 ? _originalScaleX : -_originalScaleX;
+            transform.localScale = new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
+        }
+        _animator.SetBool(RunAnim, Mathf.Abs(_moveInput) > _moveThreshold);
+        _rb.linearVelocity = new Vector2(_moveInput * _speed, _rb.linearVelocity.y);
+        
+    }
+
+}
